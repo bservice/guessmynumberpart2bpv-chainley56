cb1a5d2acf82a9c506af734c9ba5d8477fb43f13
chainley56 <31398635+chainley56@users.noreply.github.com>
2017-08-28 06:10:15 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,55 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner console;
+   console = new Scanner(System.in);
+   Random aRandom = new Random();
+   
+   int
+       prev_low = 1,
+       prev_high = 100,
+       guess = aRandom.nextInt(prev_high) + prev_low,
+       guesses = 0;
+   
+   
+
+   
+   boolean win = false;
+   
+   String feedback;
+   
+   
+
+   while(!win)
+   {
+       System.out.println("My guess is: " + guess);
+       feedback = console.nextLine();
+       //
+       
+       if(feedback.equals("Toolow") || feedback.equals("Toohigh") || feedback.equals("correct"))
+       {
+           if(feedback.equals("Toolow"))
+           {
+               prev_low = guess;
+               System.out.println(feedback);
+           }
+           else if(feedback.equals("Toohigh"))
+           {
+
+               prev_high = guess;
+               System.out.println(feedback);
+           }
+           else if(feedback.equals("correct"))
+           {
+               System.out.print("I win!");
+               win = true;
+               System.out.println(feedback);
+           }
+           
+           guess = aRandom.nextInt(prev_high) + prev_low;
+         
+       }
+   }
+
     }
 }
